b0eb5c4bc0f379bc1d3a15fa15bf6e9bdbf83bd0
HINATOWA <3126iyuki@gmail.com>
2023-11-14 10:26:31 +0900

    コントローラー　edit編集

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -86,9 +86,11 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Product $product)
     {
-        //
+        $companies = Companie::all();
+        return view('edit',compact('product'))
+        ->with('companies',$companies);
     }
 
     /**
