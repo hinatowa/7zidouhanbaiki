d05af977ae7a3176dec389978462f3ce691dc06e
HINATOWA <3126iyuki@gmail.com>
2023-12-06 12:20:53 +0900

    メーカー名に変更

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -24,13 +24,13 @@
         <div class="col-12 mb-2 mt-2">
             <div class="form-group">
             <select name="companies_id" class="form-select">
-                    <option>分類を選択してください</option>
+                    <option>メーカー名を選択してください</option>
                     @foreach ($companies as $companie)
                         <option value="{{ $companie->id }}">{{ $companie->company_name }}</option>
                     @endforeach
                 </select>
                 @error('companies_id')
-                    <span style="color:red;">分類を選択してください</span>
+                    <span style="color:red;">メーカー名を選択してください</span>
                 @enderror
             </div>
         </div>
