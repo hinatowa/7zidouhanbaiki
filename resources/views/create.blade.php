3ee5ad10c295455ff54eedf80359d0b3535b922d
HINATOWA <3126iyuki@gmail.com>
2023-12-12 18:02:38 +0900

    変更

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -60,8 +60,6 @@
         </div>
         <div class="image-table">
             <tr>
-                <!-- <td><img  name="img_path"class="ju_image" src="#" alt="image">商品画像</td>
-                <td><input id="image" type="file" name="image" ></td> -->
                 <input type="file" name="image">
             </tr>
         </div>
